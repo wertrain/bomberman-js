1014a72fcd30e4d753a392af068c7e32028c42fd
wertrain <wertrain@gmail.com>
2015-11-24 23:55:37 +0900

    add files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview サーバ側の実装。
+ */
+(function() {
+    var express = require('express');
+    var app = express();
+    app.use(express.static(__dirname + '/build'));
+
+    var http = require('http');
+    var server = http.createServer(app);
+    var io = require('socket.io').listen(server);
+
+    io.sockets.on('connection', function (socket) {
+        socket.on('connected', function () {
+            var msg = '(' + socket.id + ')' + 'が入室しました ';
+            console.log(msg);
+        });
+        socket.on('disconnect', function () {
+
+        });
+    });
+    
+    server.listen(process.env.PORT || 8080);
+})();
