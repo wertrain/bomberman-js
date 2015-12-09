20ea1bd74f7f63592f73737f9ae68053ca957b72
wertrain <wertrain@gmail.com>
2015-12-10 01:23:16 +0900

    WIP

--- client/js/game/blast.js
+++ client/js/game/blast.js
@@ -14,6 +14,11 @@
             this.blastCenterY = 0;
             this.power = 0;
             this.sprite.frame = [8, 16, 24, 32, 40];
+            
+            this.spriteTop = new enchant.Sprite(chipWidth, chipHeight);
+            this.spriteTop.image = this.image;
+            this.spriteTop.frame = [8, 16, 24, 32, 40];
+            this.addChild(this.spriteTop);
         },
         put: function(x, y) {
             this.sprite.x = x * bomberman.common.CHIP_SIZE;
